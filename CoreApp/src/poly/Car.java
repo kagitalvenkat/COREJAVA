57da4505a4ca645cf03c7f131503b6719625ae73
Rudra <kagitalvenkat@gmail.com>
2024-08-02 15:13:58 +0530

    HAS-A RELATION DEMO

---
+++ CoreApp/src/poly/Car.java
@@ -0,0 +1,15 @@
+package poly;
+
+public class Car {
+	void drive() {
+		Engine e = new Engine();
+		e.start();
+		System.out.println("Journey started");
+	}
+	
+	public static void main(String[] args) {
+		Car c = new Car();
+		c.drive();
+	}
+
+}
---
+++ CoreApp/src/poly/Engine.java
@@ -0,0 +1,13 @@
+package poly;
+
+public class Engine {
+
+	int id;
+	String name;
+	String fuelType;
+
+	void start() {
+		System.out.println("Engine started.");
+	}
+
+}
